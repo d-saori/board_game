93bb43245264befc721a8c3a1682fab2af43fab7
d-saori <60836449+d-saori@users.noreply.github.com>
2021-03-07 23:34:19 +0900

    メソッドをprivateに変更

--- othello.rb
+++ othello.rb
@@ -65,210 +65,212 @@ class Othello
     print_result()
   end
 
-  # オセロ盤の基礎作成
-  def make_board()
-    @@board = []
-    # 外枠含めた全てのマス(10*10)を置き石無しの状態にする(初期化)
-    MAX_ROW.times {
-      row = []
-      MAX_COL.times {
-        row << BLANK
+  private
+
+    # オセロ盤の基礎作成
+    def make_board()
+      @@board = []
+      # 外枠含めた全てのマス(10*10)を置き石無しの状態にする(初期化)
+      MAX_ROW.times {
+        row = []
+        MAX_COL.times {
+          row << BLANK
+        }
+        @@board << row
       }
-      @@board << row
-    }
-    
-    # 外枠を作る[行][列]
-    (0..MAX_COL - 1).each { |i|
-      @@board[0][i] = WALL # 最上行
-      @@board[MAX_ROW - 1][i] = WALL # 最下行
-    }
-    (0..MAX_ROW - 1).each { |i|
-      @@board[i][0] = WALL # 左列
-      @@board[i][MAX_COL - 1] = WALL # 右列
-    }
+      
+      # 外枠を作る[行][列]
+      (0..MAX_COL - 1).each { |i|
+        @@board[0][i] = WALL # 最上行
+        @@board[MAX_ROW - 1][i] = WALL # 最下行
+      }
+      (0..MAX_ROW - 1).each { |i|
+        @@board[i][0] = WALL # 左列
+        @@board[i][MAX_COL - 1] = WALL # 右列
+      }
 
-    # 中央に石を置く[行][列]
-    @@board[4][4] = WHITE
-    @@board[5][5] = WHITE
-    @@board[4][5] = BLACK
-    @@board[5][4] = BLACK
-  end
+      # 中央に石を置く[行][列]
+      @@board[4][4] = WHITE
+      @@board[5][5] = WHITE
+      @@board[4][5] = BLACK
+      @@board[5][4] = BLACK
+    end
 
-  # 盤面を表示
-  def print_board
-    print "  "
-    # 列座標
-    (0..MAX_COL - 1).each { |i|
-      print i.to_s + " "
-    }
-    print "\n"
-
-    # 行座標
-    (0..MAX_ROW - 1).each { |i|
-      print i.to_s + " "
-      row = @@board[i]
-      row.each { |stone|
-        print stone
+    # 盤面を表示
+    def print_board
+      print "  "
+      # 列座標
+      (0..MAX_COL - 1).each { |i|
+        print i.to_s + " "
       }
       print "\n"
-    }
-  end
 
-  # 石の配置場所が正しいか判定
-  def check_input(input)
-    input = input.chomp.split(",")
-    if input.length != 2
-      puts "石を置く場所を正しく指定してください。"
-      return false
+      # 行座標
+      (0..MAX_ROW - 1).each { |i|
+        print i.to_s + " "
+        row = @@board[i]
+        row.each { |stone|
+          print stone
+        }
+        print "\n"
+      }
     end
 
-    if !integer_string?(input[0].strip) || !integer_string?(input[1].strip)
-      puts "石を置く場所は数値で指定してください。"
-      return false
+    # 石の配置場所が正しいか判定
+    def check_input(input)
+      input = input.chomp.split(",")
+      if input.length != 2
+        puts "石を置く場所を正しく指定してください。"
+        return false
+      end
+
+      if !integer_string?(input[0].strip) || !integer_string?(input[1].strip)
+        puts "石を置く場所は数値で指定してください。"
+        return false
+      end
+
+      return true
     end
 
-    return true
-  end
+    def search_can_put_pos(turn)
+      enemy = get_enemy(turn)
+      can_put_pos_list = []
+      # 石を置いたマスの8方向(左隣のマスから時計回り)に引っくり返せる石が無いかチェック
+      directions = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
 
-  def search_can_put_pos(turn)
-    enemy = get_enemy(turn)
-    can_put_pos_list = []
-    # 石を置いたマスの8方向(左隣のマスから時計回り)に引っくり返せる石が無いかチェック
-    directions = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
-
-    (0..MAX_ROW - 1).each { |row_num|
-      (0..MAX_COL - 1).each { |col_num|
-        if @@board[row_num][col_num] != BLANK
-          next
-        end
-
-        # 置いた石の周りに相手の石があるか確認
-        directions.each { |direction|
-          can_put_flag = false
-          search_row = row_num + direction[0]
-          search_col = col_num + direction[1]
-          # 相手の石で無い場合は次の方向を確認
-          if @@board[search_row][search_col] != enemy
+      (0..MAX_ROW - 1).each { |row_num|
+        (0..MAX_COL - 1).each { |col_num|
+          if @@board[row_num][col_num] != BLANK
             next
           end
 
-          # 見つけた方向を捜査していく
-          while true
-            search_row += direction[0]
-            search_col += direction[1]
-            if @@board[search_row][search_col] != enemy && @@board[search_row][search_col] != turn
-              break
-            elsif @@board[search_row][search_col] == enemy
+          # 置いた石の周りに相手の石があるか確認
+          directions.each { |direction|
+            can_put_flag = false
+            search_row = row_num + direction[0]
+            search_col = col_num + direction[1]
+            # 相手の石で無い場合は次の方向を確認
+            if @@board[search_row][search_col] != enemy
               next
-            else
-              can_put_pos_list << [row_num, col_num]
-              can_put_flag = true
+            end
+
+            # 見つけた方向を捜査していく
+            while true
+              search_row += direction[0]
+              search_col += direction[1]
+              if @@board[search_row][search_col] != enemy && @@board[search_row][search_col] != turn
+                break
+              elsif @@board[search_row][search_col] == enemy
+                next
+              else
+                can_put_pos_list << [row_num, col_num]
+                can_put_flag = true
+                break
+              end
+            end
+
+            if can_put_flag
               break
             end
-          end
+          }
+        }
+      }
 
-          if can_put_flag
+      return can_put_pos_list
+    end
+
+    def reverse(put_row, put_col)
+      enemy = get_enemy(@@turn)
+      directions = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
+
+      @@board[put_row][put_col] = @@turn
+      directions.each { |direction|
+        reverse_pos = []
+        reverse_row = put_row + direction[0]
+        reverse_col = put_col + direction[1]
+        if @@board[reverse_row][reverse_col] != enemy
+          next
+        end
+
+        reverse_flag = false
+        reverse_pos << [reverse_row, reverse_col]
+        while true
+          reverse_row += direction[0]
+          reverse_col += direction[1]
+          if @@board[reverse_row][reverse_col] == enemy
+            reverse_pos << [reverse_row, reverse_col]
+          elsif @@board[reverse_row][reverse_col] == @@turn
+            reverse_flag = true
+            break
+          else
             break
           end
-        }
-      }
-    }
-
-    return can_put_pos_list
-  end
-
-  def reverse(put_row, put_col)
-    enemy = get_enemy(@@turn)
-    directions = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
-
-    @@board[put_row][put_col] = @@turn
-    directions.each { |direction|
-      reverse_pos = []
-      reverse_row = put_row + direction[0]
-      reverse_col = put_col + direction[1]
-      if @@board[reverse_row][reverse_col] != enemy
-        next
-      end
-
-      reverse_flag = false
-      reverse_pos << [reverse_row, reverse_col]
-      while true
-        reverse_row += direction[0]
-        reverse_col += direction[1]
-        if @@board[reverse_row][reverse_col] == enemy
-          reverse_pos << [reverse_row, reverse_col]
-        elsif @@board[reverse_row][reverse_col] == @@turn
-          reverse_flag = true
-          break
-        else
-          break
         end
-      end
 
-      # 間にあった相手の石を裏返す
-      if reverse_flag
-        reverse_pos.each { |pos|
-          @@board[pos[0]][pos[1]] = @@turn
-        }
-      end
-    }
-  end
-
-  def finish?()
-    can_put_white_list = search_can_put_pos(WHITE)
-    can_put_black_list = search_can_put_pos(BLACK)
-    if can_put_white_list.empty? && can_put_black_list.empty?
-      return true
-    end
-    return false
-  end
-
-  def print_result()
-    black_num = 0
-    white_num = 0
-    @@board.each { |row|
-      row.each { |stone|
-        if stone == BLACK
-          black_num += 1
-        elsif stone == WHITE
-          white_num += 1
+        # 間にあった相手の石を裏返す
+        if reverse_flag
+          reverse_pos.each { |pos|
+            @@board[pos[0]][pos[1]] = @@turn
+          }
         end
       }
-    }
-
-    puts "<< 勝敗結果 >>"
-    puts "#{BLACK}:#{black_num} #{WHITE}:#{white_num}"
-    if black_num > white_num
-      puts "#{BLACK}の勝利です!"
-    elsif black_num < white_num
-      puts "#{WHITE}の勝利です!"
-    else
-      puts "引き分けです。"
     end
-  end
 
-  def change_turn()
-    if @@turn == BLACK
-      @@turn = WHITE
-    else
-      @@turn = BLACK
-    end
-  end
-
-  def get_enemy(turn)
-    if turn == BLACK
-      return WHITE
-    else
-      return BLACK
-    end
-  end
-
-  def integer_string?(str)
-    begin
-      Integer(str)
-      return true
-    rescue
+    def finish?()
+      can_put_white_list = search_can_put_pos(WHITE)
+      can_put_black_list = search_can_put_pos(BLACK)
+      if can_put_white_list.empty? && can_put_black_list.empty?
+        return true
+      end
       return false
     end
-  end
+
+    def print_result()
+      black_num = 0
+      white_num = 0
+      @@board.each { |row|
+        row.each { |stone|
+          if stone == BLACK
+            black_num += 1
+          elsif stone == WHITE
+            white_num += 1
+          end
+        }
+      }
+
+      puts "<< 勝敗結果 >>"
+      puts "#{BLACK}:#{black_num} #{WHITE}:#{white_num}"
+      if black_num > white_num
+        puts "#{BLACK}の勝利です!"
+      elsif black_num < white_num
+        puts "#{WHITE}の勝利です!"
+      else
+        puts "引き分けです。"
+      end
+    end
+
+    def change_turn()
+      if @@turn == BLACK
+        @@turn = WHITE
+      else
+        @@turn = BLACK
+      end
+    end
+
+    def get_enemy(turn)
+      if turn == BLACK
+        return WHITE
+      else
+        return BLACK
+      end
+    end
+
+    def integer_string?(str)
+      begin
+        Integer(str)
+        return true
+      rescue
+        return false
+      end
+    end
 end
